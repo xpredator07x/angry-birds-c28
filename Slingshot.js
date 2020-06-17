ba022bd15930ef7c938b6c9c66bbe6d74b926fad
xpredator07x <suresh.somakumaran@effem.com>
2020-06-17 20:02:52 +0530

    constait modified

--- Slingshot.js
+++ Slingshot.js
@@ -1,8 +1,8 @@
-class Chain{
-    constructor(bodyA, bodyB){
+class Slingshot{
+    constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
-            bodyB: bodyB,
+            pointB:pointB,
             stiffness: 0.04,
             length: 10
         }
@@ -11,10 +11,18 @@ class Chain{
     }
 
     display(){
+        if(this.chain.bodyA){
         var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
+        var pointB = this.chain.pointB;
+
         strokeWeight(4);
         line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+}
+
+    fly(){
+        this.chain.bodyA=null;
+        
     }
     
 }
